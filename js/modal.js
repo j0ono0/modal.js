3fa3cb0ea0a6f1af28e52fa11e9ee31873d3fc10
John Newall <john@johnnewall.com>
2017-01-11 19:09:26 +1030

    split functions into a launcher and modal dialogues.

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,47 @@
 
+// Listener for actions that launch a modal dialogue
+var ModalLauncher = function(){
+	this.modals = $(".modal");
+	this.triggers = [];
+
+	var modal = this;
+
+	// Initialise: find all links to modals
+	for(var i=0; i < this.modals.length; i++){
+		var id = this.modals[i].id;
+		this.triggers.push(id);
+	}
+	// Listen for clicks on triggers
+	$(document).on('click',function(e){
+		if (e.target.hasAttribute("href")){
+			var hash = e.target.hash.substr(1);
+			if(modal.triggers.indexOf(hash) != -1){
+				new Modal(hash);
+			}
+		}
+	});
+}
+var modalLauncher = modalLauncher || new ModalLauncher();
+
 var Modal = function(id){
     this.id = id;
     this.elem = $("#"+id);
-    this.triggers = $("[href='#"+this.id+"']");
-    this.launch = launch;
-    this.hide = hide;
-    
+    this.close = close;
+
     var modal = this;
-    var control = control();
-    
-    function launch(){
-        this.elem.addClass('modal--open');
-    };
-    function hide(){
-        this.elem.removeClass('modal--open');
-    };
-    function control(){
-        $(document).on('click.'+this.id,function(e){
-            if(e.target.href && e.target.href.split('#')[1] == modal.id){
-                modal.launch();
-                e.preventDefault();
-            }else if(!$(e.target).hasClass('modal--open') && $(e.target).parents(".modal--open").length == 0){
-                modal.hide();
-            }
-        });
-    };
+
+    // Lauch modal on creation of object
+    this.elem.addClass('modal--open');
+    $(".modal__close--"+this.id).on('click.'+this.id,function(e){
+    	modal.close();
+    });
+	
+	function close(){
+    	this.elem.removeClass('modal--open');
+		$(".modal__close--"+this.id).off('click.'+this.id);	    
+	}
 };
 
-// Initialise modal overlays
-var modals = $(".modal")
-modals.each(function(){
-    var modal = new Modal(this.id);
-});
 
 //////////////////////////////////
 function log(msg){
